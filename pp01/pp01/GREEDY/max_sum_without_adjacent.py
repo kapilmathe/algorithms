7289781ac16073a45542fc2290a59d9008530062
Kapil Mathe <kapilmathe2000@gmail.com>
2018-06-12 00:46:15 +0530

    DP: non dp solution and comparision

--- pp01/pp01/GREEDY/max_sum_without_adjacent.py
+++ pp01/pp01/GREEDY/max_sum_without_adjacent.py
@@ -8,13 +8,25 @@ def mswa_dp(A, n):
         if cache[n]:
             return cache[n]
         else:
+            print("call made for = {0}".format(n))
             cache[n] = max(mswa(A, n-1), A[n-1]+mswa(A, n-2))
         return cache[n]
     result = mswa(A, n)
     return result
 
 
-A = [3,2,7,10]
-# A = [5,5,10,100,10,5]
+def mswa(A, n):
+    if n <= 0:
+        return 0
+    if n == 1:
+        return A[0]
+    else:
+        print("call made for = {0}".format(n))
+        return max(mswa(A, n-1), A[n-1]+mswa(A, n-2))
+
+
+# A = [3,2,7,10]
+A = [5,5,10,100,10,5]
 n = len(A)
 print(mswa_dp(A, n))
+# print(mswa(A,n))
