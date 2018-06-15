4928cae5de9059db6522a7b9ae4c5a9f018d3d42
Kapil Mathe <kapilmathe2000@gmail.com>
2018-06-16 01:33:46 +0530

    google:geekforgeek formating

--- pp01/pp01/google/subarray_given_sum.py
+++ pp01/pp01/google/subarray_given_sum.py
@@ -1,4 +1,5 @@
 # https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0
+#code
 def sags(A, n, S):
     if S > 0 and n==0:
         return -1
@@ -15,7 +16,7 @@ def sags(A, n, S):
         cumm_sum[i] = (curr_sum)
         cumm_sum_index[curr_sum] = i
         if curr_sum == S:
-            return (0,i)
+            return "{0} {1}".format(0+1,i+1)
 
     possible_sa = []
     for i in range(n):
@@ -25,11 +26,14 @@ def sags(A, n, S):
     for idx in range(len(possible_sa)):
         end, val = possible_sa[idx]
         if cumm_sum_index.__contains__(val):
-            return (cumm_sum_index[val]+1,end)
+            return "{0} {1}".format(cumm_sum_index[val]+1+1,end+1)
 
     return -1
 
+T = int(input().strip())
+for i in range(T):
+    n, S = [int(x) for x in input().strip().split(' ')]
+    A = [int(x) for x in input().strip().split(' ')]
+    print(sags(A,n,S))
 
-A = [1,2,3,4,5,6,7,8,9,10]
-S = 15
-print(sags(A, len(A), S))
+
