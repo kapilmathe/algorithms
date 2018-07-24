4c5a7ba2137858aadee15138b7e850bc32a5a0df
kapil mathe <kapilmathe2000@gmail.com>
2018-07-25 02:15:07 +0530

    greedy algorithm fro job seqencing

--- pp01/pp01/GREEDY/job_sequencing.py
+++ pp01/pp01/GREEDY/job_sequencing.py
@@ -1,1 +1,24 @@
 # https://www.geeksforgeeks.org/job-sequencing-using-disjoint-set-union/
+def sorting_key(job):
+    return job[2]
+
+def job_sequencing(d, p):
+    n = len(d)
+    jobs = zip(list(range(n)), d, p)
+    jobs = sorted(jobs, key=sorting_key)
+    buckets = [None]*n
+    print(jobs)
+    for job in jobs:
+        bucket = job[1]-1
+        if buckets[bucket] is None:
+            buckets[bucket] = job[0]
+        else:
+            while bucket > 0:
+                bucket -= 1
+                if buckets[bucket] is None:
+                    buckets[bucket] = job[0]
+                    break
+    return buckets
+
+
+
